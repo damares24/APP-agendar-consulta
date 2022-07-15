cc71b7e3142d085e558541b5e9f956538258c6f1
damares24 <damares290619@gmail.com>
2022-07-15 09:45:26 -0300

    Iniciando o  APP

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './Screen/homeScreen';
 
 export default function App() {
+  const [homeScreen, setHomeScreen] = useState()
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container}> 
+   
+     <Image source={require('./img/psf.png')}  
+     style = { styles.logo }
+      />
+
+    {/* <HomeScreen/> */}
     </View>
   );
 }
@@ -13,8 +19,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    // backgroundColor: '#fff',
+    backgroundColor: '#3C9CD2',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  logo:{
+    width: 200,
+    height: 200,
+    borderRadius: 20 / 2
   },
 });
